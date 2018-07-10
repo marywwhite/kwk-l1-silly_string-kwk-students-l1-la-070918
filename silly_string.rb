534dc474fec0916fa14ac26cf44f452c50bdf401
Mary White <maryewhite2022@gmail.com>
2018-07-10 23:32:29 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,37 +1,31 @@
 
-# Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
 
-puts "My favorite word is #{your_favorite_word}"
+your_favorite_word = "begin"
 
-# Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
+puts "My favorite word is begin"
 
-favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
+favorite_word_in_all_caps = your_favorite_word.upcase
 
-puts "MY FAVORITE WORD IS #{your_favorite_word_all_caps}"
-
-# Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
+puts "MY FAVORITE WORD IS" favorite_word_in_all_caps
 
 phrase ="IM NOT SHOUTING"
 
-lowercased_phrase = phrase # Put a . after phrase and use a String method.
+lowercased_phrase = phrase.downcase
 
 puts lowercased_phrase
 
-# Challenge 4: Count how many letters are in that big_word
-
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word.length # Put a . after big_word and use a String method
+letter_count = big_word.length
 
-puts "There are #{letter_count} in #{big_word}"
+puts "There are " letter_count "in" big_word
 
-bigger_word = big_word + "wow" + "mom" 
+bigger_word = big_word "wow" + "mom" 
 
 new_letter_count = bigger_word.size
 
-puts "There are now " + new_letter_count + " in " + bigger_word
+puts "There are now " new_letter_count " in " bigger_word
 
 proper_sentence = "i really like programming.".capitalize 
 
@@ -39,7 +33,6 @@ puts proper_sentence
 
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 
-
-sentence = sentence.gsub("A", "O").gsub("a", "o") # Add your gsub chain here
+sentence = sentence.gsub("A", "O").gsub("a", "o") 
 
 puts sentence
