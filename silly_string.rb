fb5c4f51411c034baebb878a88e5e69f7d848a40
Mary White <maryewhite2022@gmail.com>
2018-07-10 23:32:57 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,8 +1,4 @@
-def crazy_strings(first_string, second_string)
-  first_string = first_string.reverse.upcase
-  second_string = second_string.swapcase.gsub("s", "z")
-  return "#{first_string} #{second_string}"
-end
+
 
 
 your_favorite_word = "begin"
